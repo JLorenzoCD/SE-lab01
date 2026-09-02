2607dcb42abaa8e2f0ee68a6988beaf7f69ac0d4
lorenzo.canovas <lorenzo.canovas@mi.unc.edu.ar>
2026-09-02 13:59:54 -0300

    lab01

---
+++ lab01_SE.srcs/sources_1/imports/src/BCD_cnt_bank.sv
@@ -0,0 +1,57 @@
+`timescale 1ns / 1ps
+
+module BCD_cnt_bank(
+    input clk,
+    input R,
+    input en_cnt,
+    input freq_in,
+
+    output [15:0] Cout
+    );
+
+    logic carry_cnt_U;
+    logic carry_cnt_D;
+    logic carry_cnt_C;
+
+    BCD_cnt BCD_cnt_U(
+        .clk(clk),
+        .R(R),
+        .en(en_cnt),
+        .Cin(freq_in),
+
+        .carry(carry_cnt_U),
+        .Cout(Cout[3:0])
+    );
+
+    BCD_cnt BCD_cnt_D(
+        .clk(clk),
+        .R(R),
+        .en(en_cnt),
+        .Cin(carry_cnt_U),
+
+        .carry(carry_cnt_D),
+        .Cout(Cout[7:4])
+    );
+
+    BCD_cnt BCD_cnt_C(
+        .clk(clk),
+        .R(R),
+        .en(en_cnt),
+        .Cin(carry_cnt_D),
+
+        .carry(carry_cnt_C),
+        .Cout(Cout[11:8])
+    );
+
+    BCD_cnt BCD_cnt_M(
+        .clk(clk),
+        .R(R),
+        .en(en_cnt),
+        .Cin(carry_cnt_C),
+
+        .carry(), // No se utiliza el carry_out
+        .Cout(Cout[15:12])
+    );
+
+
+endmodule
